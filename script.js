fceb57d1d3151938c307e7e7d21186a427858e0e
Arthurmendescouto <153393682+Arthurmendescouto@users.noreply.github.com>
2024-02-25 13:47:29 -0300

    atualização

---
+++ script.js
@@ -0,0 +1,10 @@
+const BoxButton=document.querySelector('.linkconf')
+let white=document.querySelector('.link')
+BoxButton.addEventListener('mouseover',()=>{
+     white.style.color="black"
+  
+})
+BoxButton.addEventListener('mouseout',()=>{
+    white.style.color="white"
+ 
+})
